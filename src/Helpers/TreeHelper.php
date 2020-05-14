0fc7858976ac9de1231508de96282aadc0dcce92
joycezhang <787027175@qq.com>
2020-05-14 11:25:16 +0800

    树结构工具类

---
+++ src/Helpers/TreeHelper.php
@@ -0,0 +1,149 @@
+<?php
+// +----------------------------------------------------------------------
+// | 通用类包
+// +----------------------------------------------------------------------
+// | Copyright (c) 2020 http://www.hmall.com.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: joyecZhang <787027175@qq.com>
+// +----------------------------------------------------------------------
+
+declare (strict_types=1);
+
+namespace JoyceZ\ThinkLib\Helpers;
+
+/**
+ * 操作树节点
+ * Class TreeHelper
+ * @package JoyceZ\ThinkLib\Helpers
+ */
+class TreeHelper
+{
+    /**
+     * 获取分类id所有父级分类
+     * @param array $list 2维数组
+     * @param int $id 指定id
+     * @param string $pk 主键标识
+     * @param string $pid 父类数组
+     * @return array
+     */
+    public static function getParents(array $list, int $id, string $pk = 'id', string $pid = 'pid'): array
+    {
+        $tree = array();
+        foreach ($list as $v) {
+            if ($v[$pk] == $id) {
+                $tree[] = $v;
+                $tree = array_merge(self::getParents($list, $v[$pid]), $tree);
+            }
+        }
+        return $tree;
+    }
+
+    /**
+     * 获取分类id所有父级分类id
+     * @param array $list 维数组
+     * @param int $id 指定id
+     * @param string $pk 主键标识
+     * @param string $pid 父类数组
+     * @return array
+     */
+    public static function getParentsId(array $list, int $id, string $pk = 'id', string $pid = 'pid'): array
+    {
+        $tree = array();
+        foreach ($list as $v) {
+            if ($v[$pk] == $id) {
+                $tree[] = $v[$pk];
+                $tree = array_merge(self::getParentsId($list, $v[$pid], $pk, $pid), $tree);
+            }
+        }
+        return $tree;
+    }
+
+    /**
+     * 格式化分类，生成一维数组 ,根据path 属性
+     * @param array $list 数组
+     * @param int $root 指定根节点
+     * @param string $prefix 前缀标识
+     * @param string $pk 主键标识
+     * @param string $pid 父级标识
+     * @param string $html 前缀字符
+     * @return array
+     */
+    public static function listToTreeOne(array $list, int $root = 0, string $prefix = '', string $pk = 'id', string $pid = 'pid', string $html = 'html'): array
+    {
+        $tree = array();
+        foreach ($list as $v) {
+            if ($v[$pid] == $root) {
+                if ($v[$pid] == 0) {
+                    $level = 0;
+                } else {
+                    $level = count(self::getParentsId($list, $v[$pk], $pk, $pid));
+                }
+
+                $v[$html] = str_repeat("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", $level);
+                $v[$html] = $v[$html] ? ($v[$html] . $prefix) : $v[$html];
+                $tree[] = $v;
+                $level++;
+                $tree = array_merge($tree, self::listToTreeOne($list, $v[$pk], $prefix, $pk, $pid, $html));
+            }
+        }
+        return $tree;
+    }
+
+    /**
+     * 格式化分类--生成多维数组 ，子数组放在child 属性中
+     * @param array $list 数组
+     * @param int $root 指定根节点
+     * @param string $pk 主键标识
+     * @param string $pid 父级标识
+     * @param string $child 子级标识
+     * @return array
+     */
+    public static function listToTreeMulti(array $list, int $root = 0, string $pk = 'id', string $pid = 'pid', string $child = 'children'): array
+    {
+        $tree = array();
+        foreach ($list as $v) {
+            if ($v[$pid] == $root) {
+                $v[$child] = self::listToTreeMulti($list, $v[$pk], $pk, $pid, $child);
+                $tree[] = $v;
+            }
+        }
+        return $tree;
+    }
+
+    /**
+     * 格式化分类，生成多维数组的树
+     * @param array $list 数组
+     * @param int $root 指定根节点
+     * @param string $pk 主键标识
+     * @param string $pid 父级标识
+     * @param string $child 子级标识
+     * @return array
+     */
+    public static function listToTree(array $list, $root = 0, $pk = 'id', $pid = 'pid', $child = 'children'): array
+    {
+        // 创建Tree
+        $tree = array();
+        if (is_array($list)) {
+            // 创建基于主键的数组引用
+            $refer = array();
+            foreach ($list as $key => $data) {
+                $refer[$data[$pk]] =& $list[$key];
+            }
+            foreach ($list as $key => $data) {
+                // 判断是否存在parent
+                $parentId = $data[$pid];
+                if ($root == $parentId) {
+                    $tree[] =& $list[$key];
+                } else {
+                    if (isset($refer[$parentId])) {
+                        $parent =& $refer[$parentId];
+                        $parent[$child][] =& $list[$key];
+                    }
+                }
+            }
+        }
+        return $tree;
+    }
+}
