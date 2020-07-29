559855148837b360666357c85521e7835aa30bfd
joycezhang <787027175@qq.com>
2020-07-29 10:27:22 +0800

    过滤提交字符串，不含空格

--- src/Helpers/FiltersHelper.php
+++ src/Helpers/FiltersHelper.php
@@ -57,6 +57,26 @@ class FiltersHelper
     }
 
     /**
+     * 过滤提交字符串，不含空格
+     * @param string $string
+     * @return string|string[]|null
+     */
+    static function stringSpecialHtmlFilter(string $string)
+    {
+        $regArr = array(
+            //过滤 <script>等可能引入恶意内容或恶意改变显示布局的代码,如果不需要插入flash等,还可以加入<object>的过滤
+            "/<(\/?)(script|i?frame|style|html|body|title|link|meta|\?|\%)([^>]*?)>/isU",
+            "/(<[^>]*)on[a-zA-Z]+\s*=([^>]*>)/isU", //过滤javascript的on事件
+        );
+        $tarr = array(
+            " ", //如果要直接清除不安全的标签，这里可以留空
+            " ",
+        );
+        $string = preg_replace($regArr, $tarr, $string);
+        return $string;
+    }
+
+    /**
      * 友好显示学员运动积分排名
      * @param int $num
      * @return int|string
